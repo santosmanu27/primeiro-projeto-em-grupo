3ee38a4471445b992281786e292454c3f1f7d2ce
Leticia <bassanileticia31@gmail.com>
2026-03-30 13:26:18 -0300

    estrutura inicial

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Incial</title>
+</head>
+
+<body>
+
+    <body>
+
+        <h1>Patas & Lares</h1>
+
+        <h2>Sobre o projeto</h2>
+
+        <p>
+            O Patas & Lares é um projeto desenvolvido com o objetivo de ajudar animais
+            perdidos a voltarem para seus donos e também ajudar animais abandonados a
+            encontrarem um novo lar. Atualmente, muitos animais se perdem de seus donos
+            ou são abandonados, e muitas pessoas que encontram esses animais não sabem
+            como ajudar ou não sabem onde divulgar para encontrar o dono ou um adotante.
+        </p>
+
+        <p>
+            Além disso, muitas pessoas têm interesse em adotar um animal, mas não sabem
+            onde procurar ou não conhecem animais que estão disponíveis para adoção em
+            seu próprio bairro. Muitas vezes, esses animais estão muito próximos, mas
+            a falta de informação dificulta esse encontro entre o animal e um novo dono.
+        </p>
+
+        <p>
+            Pensando nesse problema, o nosso grupo desenvolveu o site Patas & Lares,
+            uma plataforma simples onde moradores do bairro podem cadastrar animais
+            perdidos e também cadastrar animais que estão disponíveis para adoção.
+            Dessa forma, o site ajuda tanto quem perdeu um animal quanto quem deseja adotar.
+        </p>
+
+
+        <h2>Acesse as páginas:</h2>
+
+        <p><a href="cadastrarPerdido.php">Cadastrar Animal Perdido</a></p>
+        <p><a href="cadastrarAdocao.php">Cadastrar Animal para Adoção</a></p>
+        <p><a href="listaPerdidos.php">Ver Animais Perdidos</a></p>
+        <p><a href="listaAdocao.php">Ver Animais para Adoção</a></p>
+
+        <p> Projeto desenvolvido para a disciplina de Programação Web utilizando
+            HTML, CSS e PHP, com organização baseada em SCRUM.
+        </p>
+    </body>
+
+</html>
